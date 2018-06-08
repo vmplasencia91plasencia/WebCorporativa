4c9b3b5ae8f58a0dbc891519678f52da0ae9f806
vmplasencia <vmplasencia@cujae.edu.cu>
2018-06-08 16:37:36 -0400

    hhhhh

--- WebCujae/Controllers/HomeController.cs
+++ WebCujae/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebCujae.Models;
 
 namespace WebCujae.Controllers
 {
@@ -30,6 +31,40 @@ namespace WebCujae.Controllers
         public ActionResult Noticias()
         {
             return View();
+        }
+        public ActionResult Universidad()
+        {
+            return View();
+        }
+        public ActionResult Maestrias()
+        {
+            ApplicationDbContext applicationDb = new ApplicationDbContext();
+            List<Specialty> Listado = applicationDb.Specialty.ToList();
+            applicationDb.Dispose();
+            foreach (var i in Listado)
+            {
+                if (i.type.Equals("especialidad"))
+                {
+                    Listado.Remove(i);
+                }
+            }
+
+            return View(Listado);
+        }
+        public ActionResult Especialidades()
+        {
+            ApplicationDbContext applicationDb = new ApplicationDbContext();
+            List<Specialty> Listado = applicationDb.Specialty.ToList();
+            applicationDb.Dispose();
+            foreach (var i in Listado)
+            {
+                if (i.type.Equals("maestria"))
+                {
+                    Listado.Remove(i);
+                }
+            }
+           
+            return View(Listado);
         }
     }
 }
