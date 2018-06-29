b7380b36fc869a58fafef597a2c86f46f09ea6fd
Liena <lienaperezce@gmail.com>
2018-06-29 16:21:37 -0500

    Modificaciones en la vista  el modelo de Comunidad Universitaria. Se agregaron las migraciones a la bd.

---
+++ WebCujae/Migrations/201806291531174_ComunidadUniversitariaTable.cs
@@ -0,0 +1,16 @@
+namespace WebCujae.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ComunidadUniversitariaTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
